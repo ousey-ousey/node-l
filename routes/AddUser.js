b46516f699905f7441d4317088323028ad72cf74
UoOsSe <143217955+ousey-ousey@users.noreply.github.com>
2024-09-16 21:08:53 +0300

    "Removed userController.js and inlined its functions into AddUser.js and allRoutes.js"

--- routes/AddUser.js
+++ routes/AddUser.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/customerSchema");
-const userController = require("../controllers/userController");
 var moment = require("moment");
 
-router.get("", userController.user_add_get);
+router.get("", (req, res) => {
+  res.render("user/add");
+});
+ge;
 // POST Requst
-router.post("", userController.user_post);
+router.post("", (req, res) => {
+  User.create(req.body)
+    .then(() => {
+      res.redirect("/");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 module.exports = router;
--- routes/allRoutes.js
+++ routes/allRoutes.js
@@ -2,21 +2,74 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/customerSchema");
 var moment = require("moment");
-const userController = require("../controllers/userController");
 // GET Requst
 
-router.get("/", userController.user_index_get);
+router.get("/", (req, res) => {
+  // result ==> array of objects
+  console.log("--------------------------------------------");
+  User.find()
+    .then((result) => {
+      res.render("index", { arr: result, moment: moment });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
-router.get("/edit/:id", userController.user_edit_get);
+router.get("/edit/:id", (req, res) => {
+  User.findById(req.params.id)
+    .then((result) => {
+      res.render("user/edit", { obj: result, moment: moment });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
-router.get("/view/:id", userController.user_view_get);
+router.get("/view/:id", (req, res) => {
+  // result ==> object
+  User.findById(req.params.id)
+    .then((result) => {
+      res.render("user/view", { obj: result, moment: moment });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
-router.post("/search", userController.user_search_post);
+router.post("/search", (req, res) => {
+  const searchText = req.body.searchText.trim();
+
+  User.find({ $or: [{ fireName: searchText }, { lastName: searchText }] })
+    .then((result) => {
+      res.render("user/search", { arr: result, moment: moment });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 // DELETE Request
-router.delete("/edit/:id", userController.user_delete);
+router.delete("/edit/:id", (req, res) => {
+  User.deleteOne({ _id: req.params.id })
+    .then((result) => {
+      res.redirect("/");
+      console.log(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 // PUT Requst
-router.put("/edit/:id", userController.user_put);
+router.put("/edit/:id", (req, res) => {
+  User.updateOne({ _id: req.params.id }, req.body)
+    .then((result) => {
+      res.redirect("/");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 module.exports = router;
